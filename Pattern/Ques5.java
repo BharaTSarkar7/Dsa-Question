9f15941b8e19c200b9f8314debf3957523981220
BharaT <98583588+BharaTSarkar7@users.noreply.github.com>
2023-02-24 13:58:52 +0530

    Create Ques5.java

---
+++ Pattern/Ques5.java
@@ -0,0 +1,23 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class Ques5 {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter the rows : ");
+            int n = in.nextInt();
+            for (int i = 0; i < n; i++) {
+                for (int j = n; j > i; j--) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k <= (i*2+1); k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+
+    }
+}
+
